900dee0e06b6da0e22bf50be3484d3fa5e16ee86
Maksimanja39 <kokozenko13@gmail.com>
2025-12-09 14:49:51 +0300

    weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -53,5 +53,6 @@ if __name__ == "__main__":
     threading.Thread(target=start_bot).start()
 
     # Запуск веб-сервера (Render требует открыт порт)
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 2500))
     app.run(host="0.0.0.0", port=port)
+
